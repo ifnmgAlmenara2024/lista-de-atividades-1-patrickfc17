79f1cdf09a1aa43b1cf3bc79ac32a08cd06194d3
Patrick Ferreira Carvalho <patrickferreiraC911@gmail.com>
2024-04-26 17:20:01 -0300

    Suporte a múltiplas taxas por país :sparkles:

--- 4/script.ts
+++ 4/script.ts
@@ -1,15 +1,46 @@
 const moedasSuportadas = ['BRL', 'USD', 'EUR', 'GBP'] as const;
 
 type MoedaEnum = (typeof moedasSuportadas)[number];
-type Taxas = {
+type TaxasRegiao = {
   [k in MoedaEnum]: number;
 };
+type Taxas = {
+  [k in MoedaEnum]: TaxasRegiao;
+}
+
+const taxasBrasil = {
+  BRL: 1,
+  USD: 0.2,
+  EUR: 0.18,
+  GBP: 0.16,
+} satisfies TaxasRegiao;
+
+const taxasEstadosUnidos = {
+  BRL: 5.12,
+  USD: 1,
+  EUR: 0.93,
+  GBP: 0.8
+} satisfies TaxasRegiao
+
+const taxasEuropa = {
+  BRL: 5.47,
+  USD: 1.07,
+  EUR: 1,
+  GBP: 0.86
+} satisfies TaxasRegiao
+
+const taxasReinoUnido = {
+  BRL: 6.39,
+  USD: 1.25,
+  EUR: 1.17,
+  GBP: 1
+} satisfies TaxasRegiao
 
 const taxas = {
-  BRL: 1,
-  USD: 5.5,
-  EUR: 6.5,
-  GBP: 7.5,
+  BRL: taxasBrasil,
+  USD: taxasEstadosUnidos,
+  EUR: taxasEuropa,
+  GBP: taxasReinoUnido
 } satisfies Taxas;
 
 function assertIsMoedaValida(valor: unknown): asserts valor is MoedaEnum {
@@ -23,20 +54,20 @@ function assertIsMoedaValida(valor: unknown): asserts valor is MoedaEnum {
   }
 }
 
-const moedaOrigem = window
+const valoresOrigem = window
   .prompt('Digite o valor de origem (Ex.: BRL 27.99): ')
   ?.trim()
   .split(' ');
 
-if (!moedaOrigem || moedaOrigem.length !== 2) {
+if (!valoresOrigem || valoresOrigem.length !== 2) {
   window.alert('Valor inválido');
   throw new Error();
 }
 
-const [tipo, valorBruto] = moedaOrigem;
+const [moedaOrigem, valorBruto] = valoresOrigem;
 const valorDecimal = parseFloat(valorBruto);
 
-assertIsMoedaValida(tipo);
+assertIsMoedaValida(moedaOrigem);
 
 if (isNaN(valorDecimal)) {
   window.alert('Valor inválido');
@@ -47,7 +78,7 @@ const moedaDestino = window.prompt('Digite a moeda de destino (Ex.: USD): ')?.tr
 
 assertIsMoedaValida(moedaDestino);
 
-const valorConvertido = valorDecimal * taxas[moedaDestino as MoedaEnum];
+const valorConvertido = valorDecimal * taxas[moedaOrigem][moedaDestino];
 const valorFormatado = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: moedaDestino,
